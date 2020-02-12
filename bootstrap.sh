d701ed7543eed15d08abf54f8cc9d38225c0c504
Austin Davis-Richardson <austin@onecodex.com>
2020-02-12 15:28:51 -0800

    adds bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# this script will download and install my dotfiles
+# (works on ubuntu/mac os x)
+
+set -e
+
+git clone https://github.com/audy/dotflies.git ~/.dotflies
+
+cd ~/.dotflies && ./link-files
+
+mkdir -p ~/.history
+
+# install vim plugins
+
+vim -E -c 'PlugInstall' -c 'qa!'
